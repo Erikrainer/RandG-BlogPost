5c0a04ee9b8e778a53a593f5b9a6fe61eac562df
Erik <rainer.erik70@gmail.com>
2024-05-03 09:58:01 -0700

    Creating the seeds

---
+++ seeds/BlogPost.js
@@ -0,0 +1,16 @@
+const { BlogPost } = require("../models")
+
+const blogPostData = [
+    {
+        blog_title: "Why MVC is so important",
+        blot_text: "MVC allows developers to maintain a true separation of concers, devising their code between the Model layer for data, the View layer for design and the Controller layer for application logic.",
+    },
+    {
+        blog_title: "Authentication vs. Authorization",
+        blot_text: "There is a difference between authentication and authorization. Authentication means confirming your own identity, whereas authorization means being allowed access to the system.",
+    },
+    {
+        blog_title: "Object-Relational Mapping",
+        blog_text: "I have really loved learning about ORMs. It`s really simplified the way i create queries in SQL!",
+    }
+]
