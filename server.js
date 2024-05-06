d21f9a385a043722f2e0e5c2c46c8a6c9c71ca90
Erik <rainer.erik70@gmail.com>
2024-05-06 09:35:30 -0700

    added the super secret secret session some cookies settings

--- server.js
+++ server.js
@@ -20,7 +20,12 @@ app.use(express.urlencoded({ extended: true }));
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
