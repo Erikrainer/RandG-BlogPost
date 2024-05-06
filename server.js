df38d593842facbe7a5ce9fc1a49c4b13a05b845
Erik <rainer.erik70@gmail.com>
2024-05-06 16:43:46 -0700

    got it the handlebars linked to the css, start styling the page

--- server.js
+++ server.js
@@ -43,7 +43,8 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(routes);
 
