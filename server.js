3d593cabf961cdec845aa354b609deff32e501f5
Erik <rainer.erik70@gmail.com>
2024-05-06 09:54:34 -0700

    start the handlebars

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const sess = {
   })
 };
 
-app-use(session(sess));
+app.use(session(sess));
 
 app.engine("handlebars", hbs.engine);
 
@@ -43,7 +43,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(_dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
