cc41fe19b483f1796d0be06119105c14e5b431b3
Erik <rainer.erik70@gmail.com>
2024-05-06 09:29:28 -0700

    added the comments routes

---
+++ routes/api/Comments.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+
+const { Comments } = require("../../models");
+
+const withAuth = require("../../utils/auth");
+
+router.post("/", withAuth, async (req,res) => {
+    try{
+        const newComment = await Comments.create({
+            ...req.body,
+            username: req.session.username,
+        });
+        res.status(200).json(newComment);
+    }catch(error){
+        res.status(500).json(error);
+    }
+});
+
+router.delete("/:id", withAuth, async (req,res)=> {
+    try{
+    const commentData = await Comments.destroy({
+        where: {
+            id: req.params.id,
+            username: req.session.username,
+        },
+    });
+    if(!commentData) {
+        res.status(404).json({
+            message: "No comment found with this id!!"
+        });
+        return;
+    }
+    res.status(200).json(commentData);
+}catch(error){
+    res.status(500).json(error);
+}
+});
+
+module.exports = router;
