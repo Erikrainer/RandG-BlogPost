7c1eb5803deea738d68a6e5e899d0ae15da1fd12
Erik <rainer.erik70@gmail.com>
2024-05-07 09:48:50 -0700

    change in the cookie settings

--- server.js
+++ server.js
@@ -20,8 +20,17 @@ app.use(express.urlencoded({ extended: true }));
 
 const sess = {
   secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
 };
 
 app.use(session(sess));
