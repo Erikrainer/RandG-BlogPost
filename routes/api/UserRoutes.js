96e3f2d323c173f3acb3529603c3b5221aa6d611
Erik <rainer.erik70@gmail.com>
2024-05-06 17:21:21 -0700

    added the function to login and logout, its working now and the buttons are hidding and showing correct

--- routes/api/UserRoutes.js
+++ routes/api/UserRoutes.js
@@ -3,40 +3,34 @@ const { User } = require("../../models");
 
 router.post('/', async (req, res) => {
     try {
-      const userData = await User.create(req.body);
-  
-      req.session.save(() => {
-        req.session.username = userData.username;
-        req.session.logged_in = true;
-  
-        res.status(200).json(userData);
-      });
+        const userData = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.username = userData.username;
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        });
     } catch (err) {
-      res.status(400).json(err);
+        res.status(400).json({ error: "Failed to create user." });
     }
-  });
+});
 
 router.post("/login", async (req, res) => {
-    try{
-        const userData = await User.findeOne({
+    try {
+        const userData = await User.findOne({ // Corrected method name
             where: {
                 username: req.body.username,
             },
         });
-        if(!userData) {
-            res.status(400).json({
-                message: "No user registered in the system!!"
-            });
-        return;
-        };
-        const validPassword = await userData.checkPassword(req.body.password);
+        if (!userData) {
+            return res.status(400).json({ error: "User not found." });
+        }
 
-        if(!validPassword){
-            res.status(400).json({
-                message:"Incorrect password!!"
-            });
-            return;
-        };
+        const validPassword = await userData.checkPassword(req.body.password);
+        if (!validPassword) {
+            return res.status(400).json({ error: "Incorrect password." });
+        }
 
         req.session.save(() => {
             req.session.username = userData.username;
@@ -46,18 +40,18 @@ router.post("/login", async (req, res) => {
                 message: "You are now logged in!!",
             });
         });
-    } catch(error) {
-        res.status(500).json(error);
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error." });
     }
 });
 
-router.post("/logout", (req,res) => {
-    if(req.session.logged_in) {
+router.post("/logout", (req, res) => {
+    if (req.session.logged_in) {
         req.session.destroy(() => {
             res.status(204).end();
         });
-    }else{
-        res.status(404).end();
+    } else {
+        res.status(404).json({ error: "No session found." });
     }
 });
 
